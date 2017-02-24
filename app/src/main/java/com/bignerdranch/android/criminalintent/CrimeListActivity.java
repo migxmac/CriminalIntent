72cfd2f0d103508cd91a5297a0c73b2ef59b4f62
miguel <migxmac@gmail.com>
2017-02-23 22:33:26 -0600

    Chapter 9: created abstract class SingleFragmentActivity (for reuse); changed CrimeActivity to CrimeListActivity to hold alist of Crimes

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
@@ -2,11 +2,14 @@ package com.bignerdranch.android.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity {
+/**
+ * Created by mig on 2/23/17.
+ */
 
+public class CrimeListActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        return new CrimeListFragment();
     }
 }
---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by mig on 2/23/17.
+ */
+
+public class CrimeListFragment extends Fragment {
+
+    // Nothing yet
+}
